c5ed4d82fd60af9b3d13ab6792b5539b40cc4ff7
Aryan Primasatya <aryanstya238@gmail.com>
2023-09-17 22:22:58 +0700

    Tugas PBP

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from main.views import show_main, create_product
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('create-product', create_product, name='create_product'),
 ]
